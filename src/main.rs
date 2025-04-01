d10cd2741bbb2b38be35cfc9dc5b64158eea6b6e
0xOsiris <djosiris@proton.me>
2025-04-01 11:41:34 -0700

    test: add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 use clap::Parser;
 
 mod cli;
@@ -5,11 +6,20 @@ mod client;
 mod service;
 mod utils;
 
+// TODO: Tracing/Telemetry
 #[tokio::main]
 async fn main() {
+    init_tls();
+
     let cli = cli::Cli::parse();
     if let Err(e) = cli.run().await {
         eprintln!("Fatal Error: {}", e);
         std::process::exit(1);
     }
+}
+
+fn init_tls() {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("TLS Error: Failed to install default provider");
 }
