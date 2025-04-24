94d22aa96cade14913b1f6d72f525f9a122111da
0xKitsune <0xkitsune@protonmail.com>
2025-04-24 11:18:16 -0400

    chore: move cli logic to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,13 @@
+use crate::client::{fanout::FanoutWrite, http::HttpClient};
+use crate::{
+    metrics::init_metrics_server,
+    service::{ProxyLayer, validation::ValidationLayer},
+};
+use alloy_rpc_types_engine::JwtSecret;
+use clap::Parser;
 use eyre::Context as _;
-use eyre::Result;
-use http::Uri;
+use eyre::{Result, eyre};
+use hyper::Uri;
 use jsonrpsee::{RpcModule, server::Server};
 use metrics_exporter_prometheus::PrometheusBuilder;
 use metrics_util::layers::{PrefixLayer, Stack};
@@ -8,9 +15,10 @@ use opentelemetry::trace::TracerProvider as _;
 use opentelemetry::{KeyValue, global};
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{Resource, propagation::TraceContextPropagator};
+use paste::paste;
 use rollup_boost::{HealthLayer, LogFormat};
-use rpc::{BuilderTargets, L2Targets};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::PathBuf;
 use tracing::Level;
 use tracing::error;
 use tracing::info;
@@ -19,12 +27,6 @@ use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::Layer;
 use tracing_subscriber::filter::Targets;
 use tracing_subscriber::layer::SubscriberExt;
-
-use crate::{
-    metrics::init_metrics_server,
-    service::{ProxyLayer, validation::ValidationLayer},
-};
-mod rpc;
 
 pub const DEFAULT_HTTP_PORT: u16 = 8545;
 pub const DEFAULT_METRICS_PORT: u16 = 9090;
@@ -233,3 +235,62 @@ impl Cli {
         Ok(())
     }
 }
+
+macro_rules! define_rpc_args {
+    ($(($name:ident, $prefix:ident)),*) => {
+        $(
+            paste! {
+                #[derive(Parser, Debug, Clone, PartialEq, Eq)]
+                pub struct $name {
+                    /// RPC Server 0
+                    #[arg(long, env)]
+                    pub [<$prefix _url_0>]: Uri,
+
+                    /// RPC Server 1
+                    #[arg(long, env)]
+                    pub [<$prefix _url_1>]: Uri,
+
+                    /// RPC Server 2
+                    #[arg(long, env)]
+                    pub [<$prefix _url_2>]: Uri,
+
+                    /// Hex encoded JWT secret to use for an authenticated RPC server.
+                    #[arg(long, env, value_name = "HEX")]
+                    pub [<$prefix _jwt_token>]: Option<JwtSecret>,
+
+                    /// Path to a JWT secret to use for an authenticated RPC server.
+                    #[arg(long, env, value_name = "PATH")]
+                    pub [<$prefix _jwt_path>]: Option<PathBuf>,
+
+                    /// Timeout for http calls in milliseconds
+                    #[arg(long, env, default_value_t = 1000)]
+                    pub [<$prefix _timeout>]: u64,
+                }
+
+                impl $name {
+                    fn get_jwt(&self) -> Result<JwtSecret> {
+                        if let Some(secret) = &self.[<$prefix _jwt_token>] {
+                            Ok(secret.clone())
+                        } else if let Some(path) = &self.[<$prefix _jwt_path>] {
+                            Ok(JwtSecret::from_file(path)?)
+                        } else {
+                            Err(eyre!(
+                                "No JWT secret provided. Please provide either a hex encoded JWT secret or a path to a file containing the JWT secret."
+                            ))
+                        }
+                    }
+
+                    pub fn build(&self) -> Result<FanoutWrite> {
+                        let jwt = self.get_jwt()?;
+                        let client_0 = HttpClient::new(self.[<$prefix _url_0>].clone(), jwt.clone());
+                        let client_1 = HttpClient::new(self.[<$prefix _url_1>].clone(), jwt.clone());
+                        let client_2 = HttpClient::new(self.[<$prefix _url_2>].clone(), jwt);
+                        Ok(FanoutWrite::new(vec![client_0, client_1, client_2]))
+                    }
+                }
+            }
+        )*
+    };
+}
+
+define_rpc_args!((BuilderTargets, builder), (L2Targets, l2));
