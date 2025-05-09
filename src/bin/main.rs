a042dd87eb7d1c3d7d908f885434cf06c8abb063
0xOsiris <djosiris@proton.me>
2025-05-09 14:34:13 -0700

    chore: update env parsing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use tx_proxy::cli;
-
+use dotenv::dotenv;
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     if let Err(e) = cli::Cli::parse().run().await {
         eprintln!("Fatal Error: {}", e);
         std::process::exit(1);
